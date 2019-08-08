5036b62ce231332c80328a0d025f8ed7c0d18e70
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-08 07:41:54 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Consulta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Consulta : Paciente
+    {
+        //A
+        DateTime fecha;
+        Paciente paciente;
+        string sintomas;
+        string diagnostico;
+        string tratamientos;
+
+    }
+}
